8bfa6f6ea85fcecd5d8fe7a1f1d89845f104f95c
Travis Dunn <Travisldunn@gmail.com>
2020-08-11 18:25:33 -0400

    S-W3

--- 1-patterns/3-sliding-window/practice/3-maxZero.js
+++ 1-patterns/3-sliding-window/practice/3-maxZero.js
@@ -1,26 +1,7 @@
 // Given a binary array, find the maximum number zeros in an array with one flip
 // of a subarray allowed. A flip operation switches all 0s to 1s and 1s to 0s.
 
-maxZero = (ary) => {
-  let flipped = false,
-    lastFlip,
-    start = 0,
-    max = 0;
-
-  for (let i = 0; i < ary.length; i++) {
-    if (ary[i]) {
-      if (!flipped) {
-        flipped = true;
-        lastFlip = i;
-      } else if (!ary[i - 1]) {
-        start = lastFlip + 1;
-        flipped = false;
-      }
-    }
-    max = Math.max(max, i - start + 1);
-  }
-  return max;
-};
+maxZero = (ary) => {};
 
 console.log(maxZero([0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1])); // 8
 console.log(maxZero([0, 0, 0, 1, 0, 1])); // 5

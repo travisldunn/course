1e0a7d6d6acc7887873380a7501e096e7cb4840d
Travis Dunn <Travisldunn@gmail.com>
2020-08-24 23:55:12 -0400

    SLL2

--- 4-data-structures/1-sLinkedLists/practice.js
+++ 4-data-structures/1-sLinkedLists/practice.js
@@ -143,6 +143,18 @@ class SLL {
     }
     return false;
   }
+
+  midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while (fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow;
+  }
 }
 
 const list = new SLL();

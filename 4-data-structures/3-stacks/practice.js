fe3aa47a1e247b41d219e8ec762fade8ec19d699
Travis Dunn <Travisldunn@gmail.com>
2020-08-26 14:21:40 -0400

    stack

--- 4-data-structures/3-stacks/practice.js
+++ 4-data-structures/3-stacks/practice.js
@@ -1,6 +1,40 @@
-class Node {}
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  // unshift
+  push(val) {
+    const node = new Node(val);
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      let tmp = this.first;
+      this.first = node;
+      this.first.next = tmp;
+    }
+    return ++this.size;
+  }
+
+  // shift
+  pop() {
+    if (!this.first) return null;
+    let oldFirst = this.first;
+    this.size--;
+    if (!this.size) this.last = null;
+    this.first = this.first.next;
+    return oldFirst.val;
+  }
+}
 
 let stack = new Stack();
 
@@ -8,6 +42,6 @@ stack.push("dog");
 stack.push("fart");
 stack.push("shart");
 
-// console.log(stack.pop());
+console.log(stack.pop());
 
 console.log(stack);

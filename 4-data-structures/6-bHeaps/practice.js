41c89d014d7d0a41da870ff38d0cabaf199b12ee
Travis Dunn <Travisldunn@gmail.com>
2020-09-01 16:14:14 -0400

    Heap

--- 4-data-structures/6-bHeaps/practice.js
+++ 4-data-structures/6-bHeaps/practice.js
@@ -1,32 +1,65 @@
 class Heap {
-  constructor(type = "min") {}
+  constructor(type = "min") {
+    this.type = type;
+    this.data = [];
+  }
 
   // Time Complexity: O(1)
   // Auxiliary Space Complexity: O(1)
-  compare(parent, child) {}
+  compare(p, c) {
+    if (this.type === "min") return this.data[p] <= this.data[c];
+    return this.data[p] > this.data[c];
+  }
 
   // Time Complexity: O(log(N))
   // Auxiliary Space Complexity: O(1)
-  insert(value) {}
+  insert(val) {
+    this.data.push(val);
+    this.bubbleUp();
+  }
 
   // Time Complexity: O(1)
   // Auxiliary Space Complexity: O(1)
-  peek() {}
+  peek() {
+    return this.data[0];
+  }
 
   // Time Complexity: O(log(N))
   // Auxiliary Space Complexity: O(1)
-  remove() {}
+  remove() {
+    let end = this.data.length - 1;
+    [this.data[0], this.data[end]] = [this.data[end], this.data[0]];
+    const result = this.pop();
+    this.bubbleDown();
+    return result;
+  }
 
   // Helper methods
-  swap(a, b) {}
+  swap(a, b) {
+    [this.data[a], this.data[b]] = [this.data[b], this.data[a]];
+  }
 
-  getParent(child) {}
+  getParent(child) {
+    return Math.floor((child - 1) / 2);
+  }
 
   getChild(parent) {}
 
-  bubbleUp() {}
+  bubbleUp() {
+    let c = this.data.length - 1,
+      p = Math.floor((c - 1) / 2);
 
-  bubbleDown() {}
+    while (c > 0 && !this.compare(p, c)) {
+      [this.data[c], this.data[p]] = [this.data[p], this.data[c]];
+      c = p;
+      p = Math.floor((c - 1) / 2);
+    }
+  }
+
+  bubbleDown() {
+    let parent = 0,
+      child = this.getChild(parent);
+  }
 }
 
 // heap.insert(41);

5d62e3acbcc8b2d2750488ead18304cdb0b364c3
Travis Dunn <Travisldunn@gmail.com>
2020-10-01 09:54:25 -0400

    bst valid Tree1

--- 4-data-structures/5-bST/answers/answers.js
+++ 4-data-structures/5-bST/answers/answers.js
@@ -125,6 +125,17 @@ validTree = (root) => {
   return true;
 };
 
+validTree = (root) => {
+  const nodes = [[root, -Infinity, Infinity]];
+  while (nodes.length) {
+    const [{ left, right, val }, lower, uper] = nodes.pop();
+    if (val < lower || val > uper) return false;
+    if (left) nodes.push([left, lower, val]);
+    if (right) nodes.push([right, val, uper]);
+  }
+  return true;
+};
+
 largest = (treeRoot) => {
   let current = treeRoot;
 

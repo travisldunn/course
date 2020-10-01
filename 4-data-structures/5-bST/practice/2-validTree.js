f6ef36958852b23f427a868546a83cf93f4ec06b
Travis Dunn <Travisldunn@gmail.com>
2020-10-01 09:53:40 -0400

    bst valid Tree

--- 4-data-structures/5-bST/practice/2-validTree.js
+++ 4-data-structures/5-bST/practice/2-validTree.js
@@ -1,3 +1,12 @@
-validTree = (root) => {};
+validTree = (root) => {
+  const nodes = [[root, -Infinity, Infinity]];
+  while (nodes.length) {
+    const [{ left, right, val }, lower, uper] = nodes.pop();
+    if (val < lower || val > uper) return false;
+    if (left) nodes.push([left, lower, val]);
+    if (right) nodes.push([right, val, uper]);
+  }
+  return true;
+};
 
 console.log(validTree(require("../practice").root));

57ce9787a6a9e80185faebbcc784b9ad4869430c
Travis Dunn <Travisldunn@gmail.com>
2020-10-18 12:31:27 -0400

    recursion 2

--- 2-concepts/2-recursion/practice/4-productOfArray.js
+++ 2-concepts/2-recursion/practice/4-productOfArray.js
@@ -1,4 +1,7 @@
-productOfArray = (ary) => {};
+productOfArray = (ary) => {
+  if (!ary.length) return 1;
+  return ary[0] * productOfArray(ary.slice(1));
+};
 
-// console.log(productOfArray([1, 2, 3])); // 6
-// console.log(productOfArray([1, 2, 3, 10])); // 60
+console.log(productOfArray([1, 2, 3])); // 6
+console.log(productOfArray([1, 2, 3, 10])); // 60

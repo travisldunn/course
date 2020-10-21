a24d514a2d33681f19992425d9d99a7a7e12184c
Travis Dunn <Travisldunn@gmail.com>
2020-10-20 23:07:10 -0400

    add vim

--- 2-concepts/2-recursion/practice/1-checkPalindrome.js
+++ 2-concepts/2-recursion/practice/1-checkPalindrome.js
@@ -1,7 +1,6 @@
 var isPalindrome = function (str) {
   if (str.length === 1) return true;
   if (str[0] !== str[str.length - 1]) return false;
-
   return isPalindrome(str.substr(1, str.length - 2));
 };
 
--- 2-concepts/2-recursion/practice/4-productOfArray.js
+++ 2-concepts/2-recursion/practice/4-productOfArray.js
@@ -2,6 +2,6 @@ productOfArray = (ary) => {
   if (!ary.length) return 1;
   return ary[0] * productOfArray(ary.slice(1));
 };
-
+("vin");
 console.log(productOfArray([1, 2, 3])); // 6
 console.log(productOfArray([1, 2, 3, 10])); // 60
